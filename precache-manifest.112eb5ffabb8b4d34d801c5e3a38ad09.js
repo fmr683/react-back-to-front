409e0288ffd1e26014de68621c0bc786b17bfb37
Fazlul <frm683@gmail.com>
2020-03-16 18:57:17 +0530

    Updates

---
+++ precache-manifest.112eb5ffabb8b4d34d801c5e3a38ad09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adb3fc1059bd4ef45d8dee45e9a70d5",
+    "url": "/react-back-to-front/index.html"
+  },
+  {
+    "revision": "83dc9598e8af74b684cc",
+    "url": "/react-back-to-front/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fe4b7098a168c1fa23d4",
+    "url": "/react-back-to-front/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "83dc9598e8af74b684cc",
+    "url": "/react-back-to-front/static/js/2.a354141a.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react-back-to-front/static/js/2.a354141a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe4b7098a168c1fa23d4",
+    "url": "/react-back-to-front/static/js/main.fdbe70c9.chunk.js"
+  },
+  {
+    "revision": "1864fc231097fcf9dbe0",
+    "url": "/react-back-to-front/static/js/runtime-main.2a95e888.js"
+  }
+]);
